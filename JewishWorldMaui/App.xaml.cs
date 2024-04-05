1ffab70d2e6f084ac868840c1b28274d6b85dbda
student <student@DESKTOP-L04MC2O>
2024-04-05 10:40:40 +0300

    Add project files.

---
+++ JewishWorldMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace JewishWorldMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
